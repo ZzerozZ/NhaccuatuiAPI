4741787dba4424fed4de2d8632c49340350ecc07
Dương Trọng Nghĩa <nghiaduong.170697@outlook.com.vn>
2017-08-08 21:23:39 +0700

    Add ile

--- WF_TestNhaccuatuiAPI/Form1.Designer.cs
+++ WF_TestNhaccuatuiAPI/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(12, 12);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(213, 20);
+            this.txtText.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(232, 12);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(50, 20);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "Enter";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 43);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnOK;
     }
 }
 
---
+++ WF_TestNhaccuatuiAPI/Song.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_TestNhaccuatuiAPI
+{
+    public class Song
+    {
+        //Instance:
+        private Song instance;
+        public Song Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Song();
+                return instance;
+            }
+
+            private set => instance = value;
+        }
+
+        //Properties:
+        public string Name { get => name; set => name = value; }
+        public string Path { get => path; set => path = value; }
+        public string SourceUrl { get => sourceUrl; set => sourceUrl = value; }
+        public string Author { get => author; set => author = value; }
+        public string Lyric { get => lyric; set => lyric = value; }
+        public string CoverPhotoLink { get => coverPhotoLink; set => coverPhotoLink = value; }
+        public string AlbumName { get => albumName; set => albumName = value; }
+        public int TotalTime { get => totalTime; set => totalTime = value; }
+
+        private string name;
+        private string path;
+        private string sourceUrl;
+        private string author;
+        private string lyric;
+        private string coverPhotoLink;
+        private string albumName;
+        private int totalTime;
+
+
+        //Method:
+
+        public bool Download(string SavePath)
+        {
+            try
+            {
+                string songname = SavePath + "\\" + Name + ".mp3";
+                if (!File.Exists(songname))
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile(SourceUrl, songname);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+       
+    }
+}
